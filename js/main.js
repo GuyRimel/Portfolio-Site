f0a4049125efba4a34eac8f3fcf6c101687a2418
GuyRimel <135guyrimel@gmail.com>
2022-08-10 17:16:34 -0500

    clickable words on index.html

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 // toggleText() toggles the homepage slogan
 let i = 0;
-function toggleText() {
+function toggleSlogan() {
     i++;
     i = i % 3;
-    let id = "phrase" + i;
+    let id = "slogan" + i;
 
-    document.getElementById('phrase0').style.display = "none";
-    document.getElementById('phrase1').style.display = "none";
-    document.getElementById('phrase2').style.display = "none";
+    document.getElementById('slogan0').style.display = "none";
+    document.getElementById('slogan1').style.display = "none";
+    document.getElementById('slogan2').style.display = "none";
+
+    document.getElementById(id).style.display = "inline-block";
+}
+
+// toggleText() toggles the homepage slogan
+let ii = 0;
+function toggleWord() {
+    ii++;
+    ii = ii % 3;
+    let id = "word" + ii;
+
+    document.getElementById('word0').style.display = "none";
+    document.getElementById('word1').style.display = "none";
+    document.getElementById('word2').style.display = "none";
 
     document.getElementById(id).style.display = "inline-block";
 }
